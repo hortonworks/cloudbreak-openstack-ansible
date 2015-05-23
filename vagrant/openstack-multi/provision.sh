b9e2f68fbfa86a2adaac3682af2e6e054756e2ef
Attila Kanto <attila.kanto@gmail.com>
2015-05-23 16:06:40 +0200

    do not create fs on lvm

--- vagrant/openstack-multi/provision.sh
+++ vagrant/openstack-multi/provision.sh
@@ -4,4 +4,4 @@ echo Arguments used: "$@"
 
 #ansible-playbook ../../ansible/openstack-multi.yml -i vagrant_ansible_inventory -u vagrant "$@"
 
-ansible-playbook --private-key=.vagrant/machines/controller/virtualbox/private_key --user=vagrant --connection=ssh --limit='controller' --inventory-file=.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory ../../ansible/os-controller.yml "$@"
+ansible-playbook -i vagrant_ansible_inventory  ../../ansible/openstack-multi.yml "$@"
