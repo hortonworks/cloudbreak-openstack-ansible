d93d68351eeb6e50ec0a78dfb7dc1974c3b425a0
Tamas Bihari <tamas.bihari@sequenceiq.com>
2015-07-24 10:45:24 +0200

    Mark vagrant hosts with suffix, remove unused provision.sh and dns_name parameter

--- vagrant/openstack-multi/provision.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo Arguments used: "$@"
-
-#ansible-playbook ../../ansible/openstack-multi.yml -i vagrant_ansible_inventory -u vagrant "$@"
-
-ansible-playbook -i vagrant_ansible_inventory  ../../ansible/openstack-multi.yml "$@"
